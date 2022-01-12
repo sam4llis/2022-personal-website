5d126223f07c121e2cdc12192b4608bd7ba8061c
sam3llis <samwilliamellis@gmail.com>
2022-01-12 23:28:20 +0000

    chore: initial commit
    - Initial commit for the project to support tailwindcss and fonts.
    
    - .gitignore updated to remove .projections.json
      (tpope/vim-projectionist).

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    colors: {
+      "white": "#FFFFFF",
+      "purple-050": "#B8BEED",
+      "purple-100": "#ABB4EA",
+      "purple-200": "#9FAAE7",
+      "purple-300": "#93A0E4",
+      "purple-400": "#8796E1",
+      "purple-500": "#7B8CDE",
+      "purple-600": "#6F82DB",
+      "purple-700": "#6378D8",
+      "purple-800": "#576ED5",
+      "purple-900": "#4B64D2",
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat'],
+      'lato': ['Lato'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
